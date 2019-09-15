8285ec404b8ddceedbc120be53bfb76c49aa8a25
Donna Amos <donnacamos.developer@gmail.com>
2019-09-15 17:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function driversWithRevenueOver(drivers, revenue){
-    return drivers.filter(d => d.revenue > revenue);   
+    return drivers.filter(d => d.revenue > revenue);
 }
 
 function driverNamesWithRevenueOver(drivers, revenue){
