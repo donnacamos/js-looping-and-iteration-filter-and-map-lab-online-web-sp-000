4921ca3bad08686d2f76bf92ef23cbd3db32f98a
Donna Amos <donnacamos.developer@gmail.com>
2019-09-15 17:51:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 function driversWithRevenueOver(driver, revenue){
   return drivers.filter(d => d.revenue > revenue);
 }
+
+function driverNamesWithRevenueOver(drivers, revenue){
+  return driversWithRevenueOver(drivers, revenue).map(d => d.name);
+}
+
+function exactMatch(drivers, requirements){
+  return drivers.filter(d => {
+    for(const key in requirements){
+      if(d[key] != requirements[key]){
+        return false;
+      }
+    }
+    return true;
+  })
+}
